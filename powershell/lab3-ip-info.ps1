e010aa95f0b5cd3f4f0301620d2f4bf3444d80ce
capsulepython971 <78170842+capsulepython971@users.noreply.github.com>
2021-04-20 18:56:19 -0400

    Lab 3
    Ip config script

---
+++ powershell/lab3-ip-info.ps1
@@ -0,0 +1,7 @@
+﻿get-ciminstance win32_networkadapterconfiguration | where-object IPEnabled | format-table -property index,ipaddress,ipsubnet,dnsdomain,dnsserversearchorder
+
+#index, (index)
+#ip address(es) (ipaddress)
+#subnet mask(s), (ipsubnet)
+#dns domain name, (dnsdomain)
+#and dns server., (dnsserversearchorderdnsserversearchorder)
